306ea65a123740cfc143fa13e0d0ea006763e30b
Joe Esposito <joe@joeyespo.com>
2016-04-27 22:52:46 -0400

    Py3ify.

--- gistmail.py
+++ gistmail.py
@@ -5,6 +5,8 @@ Email gist@gistmail.com with a link and get a response with that
 article's summary.
 """
 
+from __future__ import print_function, unicode_literals
+
 import re
 from email.utils import parseaddr
 
@@ -49,56 +51,55 @@ def incoming():
 
     # TODO: Check that this request is actually coming from Mandrill
 
-    print ' * INCOMING EMAIL:',
+    print(' * INCOMING EMAIL:', end='')
 
     # Get incoming message
     data = json.loads(request.form['mandrill_events'])
     if not data:
-        print ' * SKIPPING: Empty "mandrill_events" provided.'
+        print(' * SKIPPING: Empty "mandrill_events" provided.')
         return ''
     event = data[0]
     msg = event['msg'] if 'msg' in event else None
     if not msg:
-        print ' * SKIPPING: No "msg" field found.'
+        print(' * SKIPPING: No "msg" field found.')
         return ''
 
     # Get message metadata
     email = msg['from_email']
     subject = msg['subject']
     if not email:
-        print ' * SKIPPING: No "email" field found.'
+        print(' * SKIPPING: No "email" field found.')
         return ''
     if not subject:
-        print ' * SKIPPING: No "subject" field found.'
+        print(' * SKIPPING: No "subject" field found.')
         return ''
 
     # Ignore Mandrill test
     print 'Incoming email from:', email
     if email == u'example.sender@mandrillapp.com':
-        print ' * Skipping incoming test email.'
+        print(' * Skipping incoming test email.')
         return ''
 
     # Ignore malformed message
     text = msg['text'] if 'text' in msg else None
     if not text:
-        print ' * SKIPPING: No "text" field found.'
+        print(' * SKIPPING: No "text" field found.')
         return ''
 
     # Find the URL
-    text = text.encode('utf-8', 'replace').strip()
-    matches = re.search(u'(?P<url>https?://[^\s]+)', text)
+    matches = re.search('(?P<url>https?://[^\s]+)', text)
     url = matches.group('url') if matches else None
     if not url:
-        print ' * SKIPPING: No URL found in the provided text.'
+        print(' * SKIPPING: No URL found in the provided text.')
         return ''
-    print 'Summarizing:', url
+    print('Summarizing:', url)
 
     try:
         summary = summarize_page(url)
     except Exception as ex:
         if sentry:
             sentry.captureException()
-        print ' * ERROR:', type(ex), ex
+        print(' * ERROR:', type(ex), ex)
         subject = '[ERROR] ' + subject
         html = render_template('error_email.html', url=url)
     else:
@@ -106,10 +107,10 @@ def incoming():
             'summary_email.html', title=summary.title, url=summary.url,
             summaries=summary.summaries)
 
-    print 'Replying to:', email
+    print('Replying to:', email)
     email_id = send_email(email, subject, html)
 
-    print 'Reply ID:', email_id
+    print('Reply ID:', email_id)
     return ''
 
 
